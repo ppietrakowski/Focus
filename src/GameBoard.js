703c111caf32454cfc9990cb3ea9ce5e8efc3ec4
pawelpietrakowski <pawelpietrakowski22012001@gmail.com>
2022-10-21 19:37:59 +0200

    Added json parsing in GameBoard

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,5 +1,23 @@
 import { EventEmitter } from "eventemitter3";
-import { Field, FIELD_STATE_EMPTY } from "./Field";
+import { Field, FIELD_STATE_EMPTY, FIELD_STATE_PLAYER_A, FIELD_STATE_PLAYER_B, FIELD_STATE_UNPLAYABLE } from './Field'
+
+import board from "./assets/board.json"
+
+function boardToStateMask(boardState) {
+
+    if (boardState === 0)
+        return FIELD_STATE_UNPLAYABLE
+    
+    else if (boardState === 1)
+        return FIELD_STATE_EMPTY
+
+    else if (boardState === 2)
+        return FIELD_STATE_PLAYER_A
+    else if (boardState === 3)
+        return FIELD_STATE_PLAYER_B
+
+    throw new Error('Illegal board state')
+}
 
 export class GameBoard {
     static MAX_TOWER_HEIGHT = 5
@@ -9,14 +27,28 @@ export class GameBoard {
 
     constructor() {
         this.events = new EventEmitter()
-        this._grid = [new Field(FIELD_STATE_EMPTY, 0, 0)]
+        this._grid = [new Field(boardToStateMask(board.find(v => v.id === 0).state), 0, 0)]
 
         const maxSize = GameBoard.GAME_BOARD_HEIGHT * GameBoard.GAME_BOARD_WIDTH
 
-        for (let i = 1; i < maxSize; i++) {
-            this._grid[i] = new Field(FIELD_STATE_EMPTY, i % GameBoard.GAME_BOARD_WIDTH, Math.floor(i / GameBoard.GAME_BOARD_WIDTH))
+        /*
+        if (board.length < 64)
+            throw new Error('Board should have at least 64 element')
+            */
+        
+        for (let i = 1; i < board.length; i++) {
+            const field = board.find(v => v.id === i) || null
+
+            if (field === null)
+                throw new Error(`Missing object at (${(i % GameBoard.GAME_BOARD_WIDTH)}, ${Math.floor(i / GameBoard.GAME_BOARD_WIDTH)}) id=${i}`)
+
+            this._grid[i] = new Field(boardToStateMask(board[i].state), (i % GameBoard.GAME_BOARD_WIDTH), Math.floor(i / GameBoard.GAME_BOARD_WIDTH))
         }
+
+        console.log(this._grid)
     }
+
+
 
     getFieldAt(x, y) {
         if (this.isOutOfBoundsInXAxis(x) || this.isOutOfBoundsInYAxis(y))
--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import { Focus } from './Game'
 
 const gameFocus = new Focus()
 
-gameFocus._gameBoard.setFieldAt(1, 2, FIELD_STATE_PLAYER_A)
-gameFocus._gameBoard.setFieldAt(1, 1, FIELD_STATE_PLAYER_B)
-gameFocus._gameBoard.setFieldAt(3, 1, FIELD_STATE_PLAYER_B)
-gameFocus._gameBoard.setFieldAt(2, 1, FIELD_STATE_PLAYER_A)
-
-gameFocus._gameBoard.getFieldAt(3, 1)._top.push({state: FIELD_STATE_PLAYER_B})
-gameFocus._gameBoard.getFieldAt(3, 1)._top.push({state: FIELD_STATE_PLAYER_B})
-gameFocus._gameBoard.getFieldAt(3, 1)._top.push({state: FIELD_STATE_PLAYER_B})
-gameFocus._gameBoard.getFieldAt(3, 1)._top.push({state: FIELD_STATE_PLAYER_A})
-
 
 // Advanced UI :DDD
 const txt = document.createElement('div')
@@ -23,8 +13,5 @@ gameFocus.events.on(Focus.MOVED_FIELD, () => txt.innerHTML = gameFocus._gameBoar
 gameFocus.events.on(Focus.ADDED_ITEM_TO_POOL, player => console.log('player ', JSON.stringify(player), 'get a new item in pool'))
 
 gameFocus.nextTurn()
-gameFocus.moveToField(1, 1, DIRECTION_EAST, 1)
-
-gameFocus.moveToField(2, 1, DIRECTION_EAST, 1)
 
 txt.innerHTML = gameFocus._gameBoard._grid.map(v => v.state === FIELD_STATE_EMPTY ? "0" : JSON.stringify(v)).join(' , ')
