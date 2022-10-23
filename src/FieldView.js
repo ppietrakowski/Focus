82a88b29357dc88b1176cffa52392921ab634ae6
pawelpietrakowski <pawelpietrakowski22012001@gmail.com>
2022-10-23 14:53:24 +0200

    now renders possible moves

---
+++ src/FieldView.js
@@ -0,0 +1,75 @@
+import { EventEmitter } from "eventemitter3"
+import { DIRECTION_EAST, DIRECTION_NORTH, DIRECTION_SOUTH, DIRECTION_WEST, Field, FIELD_STATE_PLAYER_A, FIELD_STATE_PLAYER_B } from "./Field"
+import { Focus } from "./Game"
+
+export class FieldView {
+
+    static FIELD_UNCLICK = 'UnClick'
+    static FIELD_CLICK = 'Click'
+
+    /**
+     * 
+     * @param {Focus} game
+     * @param {Field} field 
+     * @param {*} rootElement 
+     */
+    constructor(game, field) {
+
+        this.field = field
+        this.isSelected = false
+        this.game = game
+
+        this.events = new EventEmitter()
+
+        this.domElement = document.createElement('div')
+
+        this.domElement.className = this.getUnhoveredClassName()
+
+        this.domElement.addEventListener('mouseover', e => this.onMouseOver())
+        this.domElement.addEventListener('mouseleave', e => this.onMouseLeave())
+        this.domElement.addEventListener('click', () => this.onClick())
+    }
+
+    onMouseLeave() {
+        if (this.game.currentPlayer.doesOwnThisField(this.field) && !this.isSelected)
+            this.domElement.className = this.getUnhoveredClassName()
+    }
+
+    getHoveredClassName() {
+        return (this.field.state & FIELD_STATE_PLAYER_A) ? 'playerRedFieldHovered' : (this.field.state & FIELD_STATE_PLAYER_B) ? 'playerGreenFieldHovered' : 'emptyField'
+    }
+
+    getUnhoveredClassName() {
+        return (this.field.state & FIELD_STATE_PLAYER_A) ? 'playerRedField' : (this.field.state & FIELD_STATE_PLAYER_B) ? 'playerGreenField' : 'emptyField'
+    }
+
+    onMouseOver() {
+        if (this.game.currentPlayer.doesOwnThisField(this.field) && !this.isSelected) {
+            this.domElement.className = this.getHoveredClassName()
+        }
+    }
+
+    onClick() {
+        if (this.game.currentPlayer.doesOwnThisField(this.field)) {
+            // dbl click mechanics
+            if (this.isSelected) {
+                this.domElement.className = this.getUnhoveredClassName()
+                this.events.emit(FieldView.FIELD_UNCLICK, this.field)
+            } else {
+                this.domElement.className = this.getHoveredClassName()
+                this.events.emit(FieldView.FIELD_CLICK, this.field)
+            }
+
+            this.isSelected = !this.isSelected
+        }
+    }
+
+    /**
+     * 
+     * @param {FieldView} anotherField 
+     */
+    isInRange(anotherField, range) {
+        return (anotherField.x - range.x >= this.field.x && anotherField.x + range.x <= this.field.x) && 
+                (anotherField.y - range.y >= this.field.y && anotherField.y + range.y <= this.field.y)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
-import { FIELD_STATE_PLAYER_A } from './Field'
+import { DIRECTION_NORTH, DIRECTION_WEST, FIELD_STATE_PLAYER_A } from './Field'
+import { FieldView } from './FieldView'
 import { Focus } from './Game'
 
 const gameFocus = new Focus()
 
 const board = document.getElementsByClassName('gameBoard')[0]
+const fieldsGui = []
+
+function renderInSameLine(selectedField, maxPossibleMoves, baseDirection) {
+    for (let i = 1; i <= maxPossibleMoves; i++) {
+        const offset = gameFocus.getOffsetBasedOnDirection(selectedField, baseDirection, i)
+        
+        const elements = fieldsGui.filter(v => v.isInRange(selectedField, offset))
+
+        elements.forEach(v => v.domElement.className = v.getHoveredClassName())
+    }
+}
+
+/**
+ * 
+ * @param {Field} selectedField 
+ */
+function renderPossibleMoves(selectedField) {
+    const maxPossibleMoves = selectedField.height
+
+    // north & south
+    renderInSameLine(selectedField, maxPossibleMoves, DIRECTION_NORTH)
+
+    // east & west
+    renderInSameLine(selectedField, maxPossibleMoves, DIRECTION_WEST)
+}
+
+function erasePossibleMoves() {
+    fieldsGui.forEach(v => v.domElement.className = v.getUnhoveredClassName())
+}
+
+gameFocus.events.on(Focus.MOVED_FIELD, () => fieldsGui[0].renderPossibleMoves())
 
 gameFocus.gameBoard.each(
     element => {
-        const newElement = document.createElement('div')
-        if (!element.isEmpty && element.isPlayable) {
-            newElement.className = (element.state & FIELD_STATE_PLAYER_A) ? 'playerRedField' : 'playerGreenField'
-        } else {
-            newElement.className = 'emptyField'
-        }
-
-        newElement.addEventListener('mouseover', e => {
-            if (gameFocus.currentPlayer.doesOwnThisField(element))
-                newElement.className = (element.state & FIELD_STATE_PLAYER_A) ? 'playerRedFieldHovered' : 'playerGreenFieldHovered'
-        })
-
-        newElement.addEventListener('mouseleave', e => {
-            if (gameFocus.currentPlayer.doesOwnThisField(element))
-                newElement.className = (element.state & FIELD_STATE_PLAYER_A) ? 'playerRedField' : 'playerGreenField'
-        })
-
-        board.appendChild(newElement)
+        const e = new FieldView(gameFocus, element, fieldsGui)
+        board.appendChild(e.domElement)
+        e.events.on(FieldView.FIELD_CLICK, () => renderPossibleMoves(e.field))
+        e.events.on(FieldView.FIELD_UNCLICK, () => erasePossibleMoves())
+        
+        fieldsGui.push(e)
     }
 )
