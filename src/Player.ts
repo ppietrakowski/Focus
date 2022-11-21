3fecfdd6784689673196f8d6e9600914b1994ecc
pawelpietrakowski <pawelpietrakowski22012001@gmail.com>
2022-11-21 20:27:53 +0100

    Fixed bug in ReserveView

--- src/Player.ts
+++ src/Player.ts
@@ -6,8 +6,6 @@ import { IField } from './IField'
 export interface IPlayer {
     readonly events: EventEmitter
     get state(): FieldState
-    get hasAnyPool(): boolean
-    get pooledPawns(): number
 
     doesOwnThisField(field: number | IField): boolean
     possessField(field: IField): void
@@ -17,36 +15,16 @@ export const EventPoolDecreased = 'PoolDecreased'
 export const EventPoolIncreased = 'PoolIncreased'
 
 export class Player implements IPlayer {
-    private _pooledPawns: number
     private _state: FieldState
     readonly events: EventEmitter
 
     constructor(state: number) {
         this._state = state
-        this._pooledPawns = 0
         this.events = new EventEmitter()
     }
 
     get state(): FieldState {
         return this._state
-    }
-
-    get hasAnyPool() {
-        return this._pooledPawns > 0
-    }
-
-    get pooledPawns(): number {
-        return this._pooledPawns
-    }
-
-    increasePool() {
-        this._pooledPawns++
-        this.events.emit(EventPoolIncreased)
-    }
-
-    decreasePool() {
-        this._pooledPawns--
-        this.events.emit(EventPoolDecreased)
     }
 
     doesOwnThisField(field: number | IField) {
