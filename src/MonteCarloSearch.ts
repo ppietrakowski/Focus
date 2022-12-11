68b4cdf76df697f3f11605d8f5b7d0c101a0f443
pawelp <80946292+ppietrakowski@users.noreply.github.com>
2022-12-11 17:57:01 +0100

    Extracted parts of algorithm into separate functions

--- src/MonteCarloSearch.ts
+++ src/MonteCarloSearch.ts
@@ -5,67 +5,89 @@ import { getAvailableMoves, IAvailableMoves } from './LegalMovesFactory'
 import { IPlayer } from './Player'
 
 export class MonteCarloSearch extends AiController {
-
-
-    simulationsCount = 3
-    maxCooldown = 18
+    private simulationsCount = 0
+    private maxCooldown = 18
+    private r = 0
+    private bestProbability = -1
+    private currentPlayer: IPlayer
+    private startTime: number
+    private maxWaitingTime: number
+    private firstMoveBoard: IGameBoard
+    private tempBoard: IGameBoard
+    private moves: Move[]
 
     supplyBestMove(): Move {
         return this.monteCarloSearch()
     }
 
     private monteCarloSearch(): Move {
-        let bestProbability = -1
-        const moves = getAvailableMoves(this.gameBoard, this.ownedPlayer).map(m => m.move)
-        let player = this.ownedPlayer
-        let bestMove: Move = null
+        this.bestProbability = -1
+        this.moves = getAvailableMoves(this.gameBoard, this.ownedPlayer).map(m => m.move)
+        this.currentPlayer = this.ownedPlayer
+        this.bestMove = null
 
-        for (const move of moves) {
-            let r = 0
-            let numberOfSimulations = 0
-            const startTime = Date.now()
-            const maxCooldown = startTime + (this.maxCooldown / moves.length)
-
-            while (Date.now() < maxCooldown) {
-                numberOfSimulations++
-                const board = this.gameBoard.getBoardAfterSpecifiedMove(move, player)
-                player = this.game.getNextPlayer(player)
-
-                let tempBoard = board
-
-                while (!this.hasMeetFinalCondition(tempBoard, player)) {
-                    const availableMoves = getAvailableMoves(tempBoard, player).map(m => m.move)
-                    if (tempBoard.countPlayersFields(player) === 0 && this.hasPlayerPool(tempBoard, player)) {
-                        const allPlacingMoves = availableMoves.filter(m => !!m.shouldPlaceSomething)
-                        const randomIndex = Math.floor(Math.random() * allPlacingMoves.length)
-                        const randomMove = allPlacingMoves[randomIndex]
-                        tempBoard = tempBoard.getBoardAfterSpecifiedMove(randomMove, player)
-                        player = this.game.getNextPlayer(player)
-                        continue
-                    }
-
-                    const randomIndex = Math.floor(Math.random() * availableMoves.length)
-                    const randomMove = availableMoves[randomIndex]
-                    tempBoard = tempBoard.getBoardAfterSpecifiedMove(randomMove, player)
-                    player = this.game.getNextPlayer(player)
-                }
-
-                if (tempBoard.countPlayersFields(this.game.getNextPlayer(this.ownedPlayer)) == 0) {
-                    r++
-                }
-            }
-
-            if (r / numberOfSimulations > bestProbability) {
-                bestMove = move
-                bestProbability = r / numberOfSimulations
-            }
+        for (const move of this.moves) {
+            this.simulateMove(move)
         }
 
-        if (moves[0] === bestMove) {
-            console.log('choosen first move')
+        return this.bestMove
+    }
+
+    private simulateMove(move: Move) {
+        this.r = 0
+        this.simulationsCount = 0
+        this.startTime = Date.now()
+        this.maxWaitingTime = this.startTime + (this.maxCooldown / this.moves.length)
+
+        while (Date.now() < this.maxWaitingTime) {
+            this.simulateNextMove(move)
         }
 
-        return bestMove
+        if (this.isBetterMoveThanPrevious()) {
+            this.updateToNewMove(move)
+        }
+    }
+
+    private isBetterMoveThanPrevious() {
+        return this.r / this.simulationsCount > this.bestProbability
+    }
+
+    private updateToNewMove(move: Move) {
+        this.bestMove = move
+        this.bestProbability = this.r / this.simulationsCount
+    }
+
+    private simulateNextMove(move: Move) {
+        this.simulationsCount++
+        this.firstMoveBoard = this.gameBoard.getBoardAfterSpecifiedMove(move, this.currentPlayer)
+        this.currentPlayer = this.game.getNextPlayer(this.currentPlayer)
+
+        this.tempBoard = this.firstMoveBoard
+
+        while (!this.hasMeetFinalCondition(this.tempBoard, this.currentPlayer)) {
+            this.simulateUntilWin()
+        }
+
+        if (this.tempBoard.countPlayersFields(this.game.getNextPlayer(this.ownedPlayer)) == 0) {
+            this.r++
+        }
+    }
+
+    simulateUntilWin() {
+        const availableMoves = getAvailableMoves(this.tempBoard, this.currentPlayer).map(m => m.move)
+        if (this.tempBoard.countPlayersFields(this.currentPlayer) === 0 && this.hasPlayerPool(this.tempBoard, this.currentPlayer)) {
+            const allPlacingMoves = availableMoves.filter(m => !!m.shouldPlaceSomething)
+            const randomIndex = Math.floor(Math.random() * allPlacingMoves.length)
+            const randomMove = allPlacingMoves[randomIndex]
+            this.tempBoard = this.tempBoard.getBoardAfterSpecifiedMove(randomMove, this.currentPlayer)
+            this.currentPlayer = this.game.getNextPlayer(this.currentPlayer)
+            return
+        }
+
+        const randomIndex = Math.floor(Math.random() * availableMoves.length)
+        const randomMove = availableMoves[randomIndex]
+        this.tempBoard = this.tempBoard.getBoardAfterSpecifiedMove(randomMove, this.currentPlayer)
+        this.currentPlayer = this.game.getNextPlayer(this.currentPlayer)
     }
 
     hasMeetFinalCondition(board: IGameBoard, player: IPlayer) {
